e9386c364fc4c2816596a0fae24da6548d2b6ea0
Fabian Peußer <fabian.peusser@googlemail.com>
2017-05-09 12:18:03 +0200

    ADDED DayNight

---
+++ Assets/Scripts/DayNightController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nature
+{
+    public class DayNightController : MonoBehaviour
+    {
+        public int hours = 0;
+        public int minutes = 0;
+
+        private Light light;
+
+        private void Start()
+        {
+            light = GetComponent<Light>();
+            
+        }
+
+        private void Update()
+        {
+            transform.eulerAngles = new Vector3(-60 + (hours * 11.5f) + (minutes * 0.1916f) , 0, 0);
+
+            //light.color = new Color(1, Mathf.Clamp(timeController.time.Hour / 11f, 0, 1), Mathf.Clamp(timeController.time.Hour / 14f, 0, 1));
+
+        }
+
+    }
+}
