f340b97df4997da241c2b0c58e758b598e1b6d14
Fabian Peußer <fabian.peusser@googlemail.com>
2017-05-10 11:19:26 +0200

    ADDED DayNight Time Saving ADDED LoadAndSave Game

---
+++ Assets/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Nature;
+
+public class GameController : MonoBehaviour
+{
+    public DayNightController dayNightController;
+
+    private string mapJSON;
+    private string timeJSON;
+
+
+    private void Start()
+    {
+        dayNightController.GetActualTime += SetTimeJson;
+        LoadData();
+        dayNightController.StartTime(timeJSON);
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveData();
+        PlayerPrefs.Save();
+    }
+
+    public void SetTimeJson(int hours, int min)
+    {
+        timeJSON = hours.ToString("00") + ":" + min.ToString("00");
+    }
+    public void SetMapJSON(string map)
+    {
+        mapJSON = map;
+    }
+
+
+    private void LoadData()
+    {
+        mapJSON = PlayerPrefs.GetString("Map", "");
+        timeJSON = PlayerPrefs.GetString("Time", "06:00");
+
+
+    }
+    private void SaveData()
+    {
+        PlayerPrefs.SetString("Map", mapJSON);
+        PlayerPrefs.SetString("Time", timeJSON);
+    }
+	
+}
--- Assets/Scripts/DayNightController.cs
+++ Assets/Scripts/DayNightController.cs
@@ -6,23 +6,54 @@ namespace Nature
 {
     public class DayNightController : MonoBehaviour
     {
+        public float timeScale;
         public int hours = 0;
         public int minutes = 0;
+
+        public delegate void OnNextTime(int hours, int min);
+        public event OnNextTime GetActualTime; 
 
         private Light light;
 
         private void Start()
         {
             light = GetComponent<Light>();
+
+            GetActualTime += SetSunFlare;
             
         }
 
-        private void Update()
+        public void StartTime(string json)
         {
-            transform.eulerAngles = new Vector3(-60 + (hours * 11.5f) + (minutes * 0.1916f) , 0, 0);
+            this.hours = int.Parse(json.Split(':')[0]);
+            this.minutes = int.Parse(json.Split(':')[1]);
+            StartCoroutine(NextTime());
+        }
 
-            //light.color = new Color(1, Mathf.Clamp(timeController.time.Hour / 11f, 0, 1), Mathf.Clamp(timeController.time.Hour / 14f, 0, 1));
+        private void SetSunFlare(int hours, int min)
+        {
+            transform.eulerAngles = new Vector3(-60 + (hours * 11.5f) + (min * 0.1916f), 0, 0);
+        }
 
+        IEnumerator NextTime()
+        {
+            while (true)
+            {
+                minutes += 1;
+                if (hours >= 24)
+                    hours = 0;
+
+                if (minutes >= 60)
+                {
+                    minutes = 0;
+                    hours += 1;
+                }         
+
+                GetActualTime(hours, minutes);
+
+                yield return new WaitForSeconds(timeScale / 10);
+            }
+ 
         }
 
     }
