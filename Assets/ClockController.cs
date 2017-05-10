8a386f901fdceb0dfa66cdeea43d7f09e22a9da9
Fabian Peußer <fabian.peusser@googlemail.com>
2017-05-10 16:25:08 +0200

    ADDED Clock CHANGED LoadController to Start Timer

---
+++ Assets/ClockController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Nature;
+
+public class ClockController : MonoBehaviour {
+
+    public DayNightController dayNightController;
+
+    public Text hoursText;
+    public Text minText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        dayNightController.GetActualTime += SetTime;
+	}
+	
+	private void SetTime(int hours, int min)
+    {
+        hoursText.text = hours.ToString("00");
+        minText.text = min.ToString("00");
+    }
+}
