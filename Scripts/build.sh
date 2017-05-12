1fee01be48088147318931c4e06fdfafdd8d7602
Stefan Eiermann <support@enginesk.com>
2017-05-12 17:01:21 +0200

    Update CI

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,7 +3,7 @@
 # Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
 # Change this the name of your project. This will be the name of the final executables as well.
-project="ci-build"
+project="SchoolGameProject"
 
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -25,15 +25,15 @@ echo "Attempting to build $project for OS X"
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-  -quit
+#echo "Attempting to build $project for Linux"
+#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#  -batchmode \
+#  -nographics \
+#  -silent-crashes \
+#  -logFile $(pwd)/unity.log \
+#  -projectPath $(pwd) \
+#  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+#  -quit
 
-echo 'Logs from build'
-cat $(pwd)/unity.log
+#echo 'Logs from build'
+#cat $(pwd)/unity.log
