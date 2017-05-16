6a456a35e6135e645aff8c50d3175ce57b277816
Nezhul <ansgar.barthel@yahoo.de>
2017-05-16 10:44:26 +0200

    stefan stuff

---
+++ Assets/Scripts/Build.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class Build
+{
+    public static void Perform()
+    {
+        var sceneArray = new EditorBuildSettingsScene[1];
+        sceneArray[0] = new EditorBuildSettingsScene("Assets/Fabian.unity", true);
+        EditorBuildSettings.scenes = sceneArray;
+    }
+}
