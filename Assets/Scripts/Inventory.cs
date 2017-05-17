add9f45fbc14d9fe8c9c663b4f74c636d94c71cd
stoermi <philipp-stoermer@hotmail.de>
2017-05-17 15:13:40 +0200

    CHANGED receiving random amount of items and gold ADDED event: GotNewItemsToShow => gives you a object[], Index 0: Type of Item; 1: Amount of Item; 2: Amount of Gold

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -23,9 +23,29 @@ public class Inventory : MonoBehaviour
         set { _Slots = value; }
     }
 
-    private int RandomNumber
+    private int RandomWood
+    {
+        get { return UnityEngine.Random.Range(2, 6); }
+    }
+
+    private int RandomStone
     {
         get { return UnityEngine.Random.Range(1, 4); }
+    }
+
+    private int RandomGold
+    {
+        get { return UnityEngine.Random.Range(1, 3); }
+    }
+
+    private bool HasWoodGold
+    {
+        get { return UnityEngine.Random.Range(1, 11) == 1 ? true : false; }
+    }
+
+    private bool HasStoneGold
+    {
+        get { return UnityEngine.Random.Range(1, 6) == 1 ? true : false; }
     }
 
     public Vector2 InventorySize;
@@ -47,8 +67,14 @@ public class Inventory : MonoBehaviour
             foreach (Slot keySlot in this._Slots.Keys)
                 if (this._Slots[keySlot] != null && this._Slots[keySlot].GetType() == p_Item.GetType())
                 {
-                    this._Gold += this.RandomNumber;
-                    this._Slots[keySlot].Amount += this.RandomNumber;
+                    object[] temp = this.GetAmountsOfItem(p_Item);
+
+                    this._Gold += (int)temp[2];
+                    this._Slots[keySlot].Amount += (int)temp[1];
+
+                    if (this.GotNewItemsToShow != null)
+                        this.GotNewItemsToShow(temp);
+                    
                     this.GotItem(this._Slots[keySlot]);
                     return;
                 }
@@ -64,9 +90,16 @@ public class Inventory : MonoBehaviour
             {
                 if (this._Slots[keySlot] == null)
                 {
-                    this._Gold += this.RandomNumber;
+                    object[] temp = this.GetAmountsOfItem(p_Item);
+
+                    p_Item.Amount = (int)temp[1];
+                    this._Gold += (int)temp[2];
                     this._Slots[keySlot] = p_Item;
                     this._Slots.Where(s => s.Value == p_Item).FirstOrDefault().Key.GetComponent<Slot>().MyItem = p_Item;
+
+                    if (this.GotNewItemsToShow != null)
+                        this.GotNewItemsToShow(temp);
+
                     this.GotItem(p_Item);
                     return;
                 }
@@ -85,16 +118,59 @@ public class Inventory : MonoBehaviour
         //if (query.Count() == 1)
         //    this._Items.Remove(query.ToArray()[0]);
     }
-    
+
+    private object[] GetAmountsOfItem(Item p_Item)
+    {
+        object[] temp = new object[3];
+
+        temp[0] = p_Item.GetType();
+
+        if (p_Item is Wood)
+        {
+            if (this.HasWoodGold)
+            {
+                int gold = this.RandomGold;
+                temp[1] = this.RandomWood * gold;
+                temp[2] = gold;
+            }
+
+            else
+            {
+                temp[1] = this.RandomWood;
+                temp[2] = 0;
+            }
+        }
+
+        else
+        {
+            if (this.HasStoneGold)
+            {
+                int gold = this.RandomGold;
+                temp[1] = this.RandomStone * gold;
+                temp[2] = gold;
+            }
+
+            else
+            {
+                temp[1] = this.RandomStone;
+                temp[2] = 0;
+            }
+        }
+
+        return temp;
+    }
+
     #endregion // Methods
 
     #region Events & Handler
 
     public delegate void ItemEventHandler(Item p_NewItem);
+    public delegate void ItemUIEventHandler(object[] p_AmountsOfItems);
     
     public event ItemEventHandler GotItem;
+    public event ItemUIEventHandler GotNewItemsToShow;
     //public event ItemEventHandler RemovedItem;
-
+    
     public void GettingItem(GameObject p_ItemHolder)
     {
         string itemHolderName = p_ItemHolder.tag == "Stone" ? "Rock" : p_ItemHolder.tag;
