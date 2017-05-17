6d309056f1e89ddb2f3e06f1a413cb311c0a0991
Fabian Peußer <fabian.peusser@gmail.com>
2017-05-17 19:27:26 +0200

    ADDED ItemsFound Interface (functional)

---
+++ Assets/LootController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootController : MonoBehaviour
+{ 
+    public GameObject ItemPrefab;
+    public GameObject ItemContent;
+
+    public Sprite wood;
+    public Sprite stone;
+    public Sprite gold;
+
+    private Inventory playerInventory;
+
+    private void Start()
+    {
+        playerInventory = GameObject.FindWithTag("Player").GetComponent<Inventory>();
+        playerInventory.GotNewItemsToShow += ItemsFound;
+
+        Hide();
+    }
+
+    private void ItemsFound(object[] Items)
+    {
+        GameObject item = Instantiate(ItemPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+        item.transform.SetParent(ItemContent.transform);
+        if(Items[0].ToString() == "Wood")
+        {
+            item.GetComponent<LootItem>().SetMe(wood, (int)Items[1]);
+        }
+        else
+        {
+            item.GetComponent<LootItem>().SetMe(stone, (int)Items[1]);
+        }
+
+        for(int i = 0; i < (int)Items[2]; i++)
+        {
+            GameObject goldItem = Instantiate(ItemPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+            goldItem.transform.SetParent(ItemContent.transform);
+            goldItem.GetComponent<LootItem>().SetMe(gold, 0);
+        }
+
+        Show();
+
+        StartCoroutine(HideAfterTime(3.0f));
+
+        ClearContent();
+    }
+
+    private void ClearContent()
+    {
+        foreach(Transform child in ItemContent.transform)
+        {
+            Destroy(child.gameObject, 3.0f);
+        }
+
+    }
+
+    private IEnumerator HideAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        Hide();
+    }
+
+    private void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    private void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/LootItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LootItem : MonoBehaviour
+{
+    public Image itemImage;
+    public Text itemAmount;
+
+    public void SetMe(Sprite sprite, int amount)
+    {
+        itemImage.sprite = sprite;
+
+        if(amount == 0)
+            itemAmount.text = "";
+
+        else
+            itemAmount.text = amount.ToString();
+
+    }
+	
+}
